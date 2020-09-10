be7da9dad2002b58a64332d9169943126e4de3a2
John Nederveld <john.nederveld@gmail.com>
2020-09-10 18:26:50 -0400

    added variable 'selections' for use in ffunction, based on selected pw criteria

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-
-
-var enter;
-var confirmNum;
-var confirmChars;
-var confirmUpper;
-var confirmLowercase;
-
 // characters
 var chars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // numbers
@@ -14,6 +6,16 @@ var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//variables to confirm selections
+var enter;
+var confirmNum;
+var confirmChars;
+var confirmUpper;
+var confirmLowercase;
+
+//empty variable which will be updated depending on what selections user makes
+var selections;
 
 //password variable to add to page
 var pw = document.querySelector("#password").innerHTML = pw;
