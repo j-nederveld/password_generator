4b34cdb73c7fa27f8a0c9d34ade77f3f45301837
John Nederveld <john.nederveld@gmail.com>
2020-09-10 14:33:24 -0400

    initial push arrays created for variables

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
 var enter;
 var confirmNum;
 var confirmChars;
 var confirmUpper;
 var confirmLowercase;
- 
+
 // characters
 chars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // numbers
@@ -13,4 +15,17 @@ lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 
 upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
