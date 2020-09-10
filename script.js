26bc4324ee99d523a0a05d38980a5ddf97a0137c
John Nederveld <john.nederveld@gmail.com>
2020-09-10 18:13:46 -0400

    added function for randomly selecting values from each array

--- script.js
+++ script.js
@@ -7,13 +7,34 @@ var confirmUpper;
 var confirmLowercase;
 
 // characters
-chars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var chars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 // numbers
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // upper/lowercase
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//password variable to add to page
+var pw = document.querySelector("#password").innerHTML = pw;
+
+//function to select random char
+function random_char() {   
+      chars[Math.floor(Math.random() * chars.length)]; 
+}
+//function for random uppercase letter
+function random_upper() {   
+    upper[Math.floor(Math.random() * upper.length)]; 
+}
+//function for random lowercase letter
+function random_lower() {  
+    lower[Math.floor(Math.random() * lower.length)]; 
+}
+//function for random number
+function random_num() { 
+ 
+    number[Math.floor(Math.random() * number.length)]; 
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
